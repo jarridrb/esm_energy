0b43785e85533ae12da26bcf7a4e8e178959f2a3
Jarrid Rector-Brooks <jarrid@Jarrids-MacBook-Pro.local>
2023-10-30 17:45:53 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="esm_reward",
+    version=VERSION,
+    description="Code from the ESM repo to give reward for esm log likelihood task",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+    packages=['esm_reward']
+)
+
